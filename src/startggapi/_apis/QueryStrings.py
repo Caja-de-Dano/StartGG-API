a209b17915201a8355547a117db4112dad8a9f31
Atomme1 <tristan@lataniere.net>
2025-02-22 14:17:20 +0100

    [FEAT] Added new queries for getting more data about a tournament + get sets in phaseGroup in phase

--- src/startggapi/_apis/QueryStrings.py
+++ src/startggapi/_apis/QueryStrings.py
@@ -354,37 +354,37 @@ videogame_details_by_slug_query = """
 # This query gets a lot of information from all events of tournaments
 # Anthology : tournament -> events -> phases (or in other terms pools like "Pool A1")
 tournament_query_by_slug_heavy = """
-query GetTournamentInfo($tourneySlug: String!) {
+  query GetTournamentInfo($tourneySlug: String!) {
     tournament(slug: $tourneySlug) {
+      id
+      name
+      slug
+      shortSlug
+      numAttendees
+      startAt
+      events {
         id
-        name
         slug
-        shortSlug
-        numAttendees
-        startAt
-        events {
-            id
-            slug
-            name
-            numEntrants
-            videogame {
-              id
-              displayName
-              slug
-            }
-            phases {
-                id
-                name
-                phaseGroups(query: { page: 1, perPage: 100 }) {
-                    nodes {
-                        id
-                        displayIdentifier
-                    }
-                }
-            }
+        name
+        numEntrants
+        videogame {
+          id
+          displayName
+          slug
         }
+        phases {
+          id
+          name
+          phaseGroups(query: { page: 1, perPage: 100 }) {
+            nodes {
+              id
+              displayIdentifier
+            }
+          }
+        }
+      }
     }
-}
+  }
 """
 
 # This query gets sets from a phase group
@@ -392,24 +392,24 @@ query GetTournamentInfo($tourneySlug: String!) {
 # There will always be a phase group even if your tournament only have one tree, so you can always use this to get sets
 # from an event
 sets_query_from_phaseGroup_from_phase = """
- query PhaseGroupSets($phaseId: ID!, $page: Int!){
-        phaseGroup(id: $phaseId){
-            sets(page: $page, perPage: 100, sortType: STANDARD) {
-      pageInfo{
-        totalPages
-      }
-                nodes {
-                    id
-                    fullRoundText
-                    identifier
-                    slots {
-                        entrant {
-                            id
-                            name
-                        }
-                    }
-                }
-            }
+  query PhaseGroupSets($phaseId: ID!, $page: Int!){
+    phaseGroup(id: $phaseId){
+      sets(page: $page, perPage: 100, sortType: STANDARD) {
+        pageInfo {
+          totalPages
         }
+          nodes {
+            id
+            fullRoundText
+            identifier
+            slots {
+              entrant {
+                id
+                name
+              }
+            }
+         }
+      }
     }
+  }
 """
